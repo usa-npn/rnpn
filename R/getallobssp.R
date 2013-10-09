8f4e84910f2351b58e8647a66847b3bad3357fe0
schamberlain <myrmecocystus@gmail.com>
2013-10-08 23:55:41 -0700

    updating getallobssp to be mo better

--- R/getallobssp.R
+++ R/getallobssp.R
@@ -14,28 +14,74 @@
 #' # Lookup names
 #' lookup_names(name='Pinus', type='genus')
 #' 
-#' # Get data on some species
+#' # Get data on one species
+#' getallobssp(speciesid = 52, startdate='2008-01-01', enddate='2011-12-31')
+#' 
+#' # Get data on two species
 #' getallobssp(speciesid = c(52, 53), startdate='2008-01-01', enddate='2011-12-31')
+#' 
+#' # Get data on one species, convert to X
+#' out <- getallobssp(speciesid = 52, startdate='2008-01-01', enddate='2011-12-31')
+#' npn_todf(out)
 #' }
-getallobssp <- function(speciesid = NA, startdate = NA, enddate = NA, ..., 
-                        curl = getCurlHandle() )
+getallobssp <- function(speciesid = NULL, startdate = NULL, enddate = NULL, callopts=list())
 {
-  url = 'https://www.usanpn.org/npn_portal/observations/getAllObservationsForSpecies'
-  method = 'getAllObservationsForSpecies'
-  url2 <- paste(url, '.json', sep='')
-  args <- list()
-  if(!is.na(speciesid[1]))
-    for(i in 1:length(speciesid)) {
-      args[paste('species_id[',i,']',sep='')] <- speciesid[i]
-    }
-  if(!is.na(startdate))
-    args$start_date <- startdate
-  if(!is.na(enddate))
-    args$end_date <- enddate
-  tt <- getForm(url2,
-    .params = args,
-#     ...,
-    curl = curl)
-  out <- fromJSON(tt)
-  ldply(out, )
+  if(is.null(speciesid))
+    stop("You must provide a speciesid")
+  taxa <- taxonlist[as.numeric(as.character(taxonlist[,"species_id"])) %in% speciesid,c("species_id","genus","species")]
+  taxa$species_id <- as.numeric(as.character(taxa$species_id))
+#   taxa <- split(taxa, taxa$species_id)
+#   taxa <- lapply(taxa, function(x) list(species_id=as.numeric(as.character(x$species_id)), name=paste(x$genus,x$species,sep=" ")))
+  
+  url = 'https://www.usanpn.org/npn_portal/observations/getAllObservationsForSpecies.json'
+  args <- compact(list(start_date=startdate, end_date=enddate))
+  for(i in seq_along(speciesid)){
+    args[paste('species_id[',i,']',sep='')] <- speciesid[i]
+  }
+  out <- GET(url, query=args, callopts)
+  stop_for_status(out)
+  tt <- content(out)
+  station_list <- data.frame(do.call(rbind, tt$station_list))
+  phenophase_list <- data.frame(do.call(rbind, tt$phenophase_list))
+  foo <- function(x){
+    tmp <- list(date=x$date, 
+                station_id=x$stations[[1]]$station_id,
+                species_id=x$stations[[1]]$species_ids[[1]]$species_id,
+                phenophase=data.frame(do.call(rbind, x$stations[[1]]$species_ids[[1]]$phenophases)))
+    do.call(data.frame, tmp)
+  }
+  temp <- lapply(tt$observation_list, foo)
+  data <- do.call(rbind, temp)
+  names(data)[4:5] <- c("phenophase_id","phen_seq")
+  
+  new("npn", taxa=taxa, stations=station_list, phenophase = phenophase_list, data = data)
 }
+
+setClass("npn", slots=list(taxa="data.frame",
+                           stations="data.frame", 
+                           phenophase="data.frame", 
+                           data="data.frame"))
+
+#' Coerce elements of output from a call to occ to a single data.frame
+#' @param x An object of class occdat
+#' @param minimal Default is FALSE
+#' @return An object of class npnsp (for npn spatial)
+#' @export
+npn_todf <- function(x, minimal=FALSE)
+{
+  if(!is(x,"npn"))
+    stop("Input object must be of class npn")
+  
+  dat <- merge(x@stations, x@data, by="station_id")
+  dat <- merge(dat, x@taxa, by="species_id")[,-c(1,2)]
+  dat <- merge(dat, x@phenophase, by="phenophase_id")[,-1]
+  dat <- transform(dat, sciname = paste(genus, species, sep=" "))
+  dat <- data.frame(sciname=dat$sciname, latitude=dat$latitude, longitude=dat$longitude, 
+               dat[,!names(dat)%in%c("sciname","latitude","longitude")])
+  if(minimal)
+    dat <- dat[,c("sciname","latitude","longitude")]
+  
+  new("npnsp", data=dat)
+}
+
+setClass("npnsp", slots=list(data="data.frame"))
