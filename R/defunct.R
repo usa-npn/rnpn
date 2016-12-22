b26e9941072ecfdac26c1bc36387d5a6c31c8c07
Scott Chamberlain <myrmecocystus@gmail.com>
2016-12-22 11:07:56 -0800

    fix #18 removed npn_obsspbyday, bumped patch version

---
+++ R/defunct.R
@@ -0,0 +1,17 @@
+#' This function is defunct.
+#' @export
+#' @rdname npn_obsspbyday-defunct
+#' @keywords internal
+npn_obsspbyday <- function(...) {
+  .Defunct(package = "rnpn",
+           msg = "the npn_obsspbyday() function no longer works, removed")
+}
+
+#' Defunct functions in rnpn
+#'
+#' \itemize{
+#'  \item \code{\link{npn_obsspbyday}}: Removed.
+#' }
+#'
+#' @name rnpn-defunct
+NULL
