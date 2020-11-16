23b9f7dbdfee0a7a12e067dd98f20198bc6034a1
npnlee85 <lee@usanpn.org>
2020-11-16 14:46:54 -0700

    Cleaning up dependencies, re-checking package, re-building docs

--- R/rnpn-package.R
+++ R/rnpn-package.R
@@ -2,11 +2,10 @@
 #'
 #' @description This package allows for easy access to the National Phenology Network's Data API. To learn more, take a look at the vignettes.
 #' events that occur at specific times.
-#' @importFrom stats setNames
-#' @importFrom data.table rbindlist setDF
+#' @importFrom data.table rbindlist
 #' @importFrom httr GET stop_for_status content
 #' @importFrom jsonlite fromJSON
-#' @importFrom plyr llply ldply ddply summarise rbind.fill
+#' @importFrom plyr rbind.fill
 #' @importFrom magrittr "%>%"
 #' @importFrom utils URLencode download.file object.size write.table
 #' @name rnpn-package
