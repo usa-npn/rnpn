cbfc9b77a6d89d4d4c2a4f6a70e92943cc2edaa0
Eric Scott <scottericr@gmail.com>
2024-12-13 10:42:21 -0700

    generate inst/CITATION for more detailed `citation("rnpn")` results

--- R/release-reminders.R
+++ R/release-reminders.R
@@ -3,7 +3,8 @@
 #https://usethis.r-lib.org/reference/use_release_issue.html
 release_bullets <- function() {
 c(
-  "Update CITATION.cff with `cffr::cff_create(dependencies = FALSE) |> cff::cff_write()`",
+  "Update CITATION.cff with `cffr::cff_write(cffr::cff_create(dependencies = FALSE))`",
+  "Update inst/CITATION with `cffr::cff_write_citation(cffr::cff_create(), file = 'inst/CITATION')`",
   "Update codemeta.json with `codemetar::write_codemeta()`"
   #TODO add reminder to pre-compute vignettes
 )
