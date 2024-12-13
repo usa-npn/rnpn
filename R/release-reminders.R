a0031609a2fbcc4af291ba4449669814d1b38639
Eric Scott <scottericr@gmail.com>
2024-12-13 10:54:45 -0700

    add release reminders

--- R/release-reminders.R
+++ R/release-reminders.R
@@ -3,9 +3,6 @@
 #https://usethis.r-lib.org/reference/use_release_issue.html
 release_bullets <- function() {
 c(
-  "Update CITATION.cff with `cffr::cff_write(cffr::cff_create(dependencies = FALSE))`",
-  "Update inst/CITATION with `cffr::cff_write_citation(cffr::cff_create(), file = 'inst/CITATION')`",
-  "Update codemeta.json with `codemetar::write_codemeta()`"
-  #TODO add reminder to pre-compute vignettes
+ "Run code in inst/release-tasks.R"
 )
 }
