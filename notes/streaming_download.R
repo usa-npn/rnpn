24190cdc5dc3f40bf78eeacce1d936560bda4fe6
Eric Scott <scottericr@gmail.com>
2025-01-07 16:57:07 -0700

    figure out mixed numeric and character parsing

--- notes/streaming_download.R
+++ notes/streaming_download.R
@@ -11,7 +11,7 @@ some_data <- npn_download_status_data(
 
 url <- "https://services.usanpn.org/npn_portal/observations/getObservations.ndjson"
 
-#this gets put into the query with curl::handle_setform()—I'm not sure what that does
+#this gets put into the query with curl::handle_setform()—httr2 equivalent is req_body_form()
 query <- list(request_src = "Unit%20Test", climate_data = "0", `species_id[1]` = "6",
               start_date = "2013-01-01", end_date = "2013-12-31", customrequest = "POST")
 download_path <- NULL
@@ -30,17 +30,18 @@ con <- req_perform_connection(req)
 continue <- TRUE
 out <- tibble::tibble()
 while(isTRUE(continue)) {
-  resp <- resp_stream_lines(con, lines = 5000) #works if I stream 1 line at a time, but not 10
+  resp <- resp_stream_lines(con, lines = 5000)
   continue <- length(resp) > 0
 
   if (continue) {
-    df_line <-
-      purrr::map(resp, \(resp_line) {
-        yyjsonr::read_ndjson_str(resp_line, type = "df") %>%
-          mutate(across(everything(), \(x) ifelse (x == -9999, NA, x)))
-      }) %>% purrr::list_rbind()
-
-    out <- dplyr::bind_rows(out, df_line)
+    df_chunk <-
+      #paste lines into single string
+      paste0(resp[nzchar(resp) != 0], collapse = "\n") %>%
+      #default to character when mixed numeric and character
+      yyjsonr::read_ndjson_str(type = "df", nprobe = -1, promote_num_to_string = TRUE) %>%
+      #replace missing data indicator with NA
+      mutate(across(where(is.numeric), \(x) ifelse(x == -9999, NA_real_, x)))
+    out <- dplyr::bind_rows(out, df_chunk)
   }
 }
 dim(out)
