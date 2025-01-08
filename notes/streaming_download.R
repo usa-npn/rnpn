7bb2cd5fc42fd48b48b8cf06f472268260b1b39a
Eric Scott <scottericr@gmail.com>
2025-01-08 15:38:44 -0700

    figuring out how to replace curl

--- notes/streaming_download.R
+++ notes/streaming_download.R
@@ -2,11 +2,11 @@ library(httr2)
 library(tidyverse)
 
 #attempting to replicate this test:
-some_data <- npn_download_status_data(
-  "Unit Test",
-  c(2013),
-  species_ids = c(6)
-)
+# some_data <- npn_download_status_data(
+#   "Unit Test",
+#   c(2013),
+#   species_ids = c(6)
+# )
 
 
 url <- "https://services.usanpn.org/npn_portal/observations/getObservations.ndjson"
@@ -21,8 +21,18 @@ six_bloom_raster <- NULL
 agdd_layer <- NULL
 additional_layers <- NULL
 
-req <- base_req |>
-  httr2::req_url_path_append("observations", "getObservations.ndjson") %>%
+wrangle_resp_chunk <- function(resp) {
+  #paste lines into single string
+  paste0(resp[nzchar(resp) != 0], collapse = "\n") %>%
+    #default to character when mixed numeric and character
+    yyjsonr::read_ndjson_str(type = "df", nprobe = -1, promote_num_to_string = TRUE) %>%
+    #replace missing data indicator with NA
+    dplyr::mutate(dplyr::across(where(is.numeric), \(x) ifelse(x == -9999, NA_real_, x))) %>%
+    dplyr::mutate(dplyr::across(where(is.character), \(x) ifelse(x == "-9999", NA_character_, x)))
+}
+
+req <- httr2::request(url) |>
+  httr2::req_user_agent("rnpn (https://github.com/usa-npn/rnpn/)") %>%
   httr2::req_method("POST") %>%
   httr2::req_body_form(!!!query)
 
@@ -30,21 +40,14 @@ con <- httr2::req_perform_connection(req)
 
 continue <- TRUE
 out <- tibble::tibble()
-while(isTRUE(continue)) {
+while (isTRUE(continue)) {
   resp <- httr2::resp_stream_lines(con, lines = 5000)
+  df_chunk <- wrangle_resp_chunk(resp)
+  out <- dplyr::bind_rows(out, df_chunk)
   continue <- length(resp) > 0
-
-  if (continue) {
-    df_chunk <-
-      #paste lines into single string
-      paste0(resp[nzchar(resp) != 0], collapse = "\n") %>%
-      #default to character when mixed numeric and character
-      yyjsonr::read_ndjson_str(type = "df", nprobe = -1, promote_num_to_string = TRUE) %>%
-      #replace missing data indicator with NA
-      dplyr::mutate(dplyr::across(where(is.numeric), \(x) ifelse(x == -9999, NA_real_, x))) %>%
-      dplyr::mutate(dplyr::across(where(is.character), \(x) ifelse(x == "-9999", NA_character_, x)))
-    out <- dplyr::bind_rows(out, df_chunk)
-  }
 }
 dim(out)
 close(con)
+
+
+
