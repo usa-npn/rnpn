ffdd07ca811df79d7d93cfb07d19c7ec311d036f
Eric Scott <scottericr@gmail.com>
2025-03-25 09:08:29 -0700

    remove unused funs

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,2 @@
-
+#creates an environment used for in-memory cacheing
 pkg.env <- new.env(parent = emptyenv())
-
-#' Set Environment
-#'
-#' By default this library will call the NPN's production services
-#' but in some cases it's preferable to access the development web services
-#' so this function allows for manually setting the web service endpoints
-#' to use DEV instead. Just pass in "dev" to this function to change the
-#' endpoints to use.
-#' @param env The environment to use. Should be "ops" or "dev"
-#' @export
-npn_set_env <- function (env = "ops") {
-  pkg.env$remote_env <- env
-}
-
-get_test_env <- function() {
-  return("dev")
-}
